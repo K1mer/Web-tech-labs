b017432b12360f8e1e44366fc9a250dea57979a9
shit, it's not google <k1merpro@gmail.com>
2022-03-16 23:30:45 +0700

    2.2

---
+++ Backend/Web2.2/Web2.2/Controllers/CalcController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web2._2.Services;
+
+namespace Web2._2.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Manual(int a, int b, Operation? operation = null)
+        {
+            switch (operation)
+            {
+                case Operation.Add:
+                    ViewData["Result"] = CalcService.Add(a, b);
+                    break;
+                case Operation.Sub:
+                    ViewData["Result"] = CalcService.Sub(a, b);
+                    break;
+                case Operation.Mult:
+                    ViewData["Result"] = CalcService.Mult(a, b);
+                    break;
+                case Operation.Div:
+                    ViewData["Result"] = CalcService.Div(a, b);
+                    break;
+            }
+            return View();
+        }
+
+        public IActionResult ManualWithSeparateHandlers(int a, int b, Operation? operation = null)
+        {
+            switch (operation)
+            {
+                case Operation.Add:
+                    return Act_Add(a, b);
+                case Operation.Sub:
+                    return Act_Sub(a, b);
+                case Operation.Mult:
+                    return Act_Mult(a, b);
+                case Operation.Div:
+                    return Act_Div(a, b);
+                default:
+                    return View();
+            }
+        }
+
+        private IActionResult Act_Add(int a, int b)
+        {
+            ViewData["Result"] = CalcService.Add(a, b);
+            return View();
+        }
+
+        private IActionResult Act_Sub(int a, int b)
+        {
+            ViewData["Result"] = CalcService.Sub(a, b);
+            return View();
+        }
+
+        private IActionResult Act_Mult(int a, int b)
+        {
+            ViewData["Result"] = CalcService.Mult(a, b);
+            return View();
+        }
+
+        private IActionResult Act_Div(int a, int b)
+        {
+            ViewData["Result"] = CalcService.Div(a, b);
+            return View();
+        }
+
+        public IActionResult ModelBindingInParameters()
+        {
+            return View();
+        }
+
+        public IActionResult ModelBindingInSeparateModel()
+        {
+            return View();
+        }
+    }
+}
---
+++ Backend/Web2.2/Web2.2/Services/CalcService.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web2._2.Services
+{
+    public enum Operation
+    {
+        [Display(Name = "+")]
+        Add,
+        [Display(Name = "-")]
+        Sub,
+        [Display(Name = "*")]
+        Mult,
+        [Display(Name = "/")]
+        Div
+    }
+
+    public static class ServiceProviderExtensions
+    {
+        public static void AddCalcService(this IServiceCollection services)
+        {
+            services.AddTransient<CalcService>();
+        }
+    }
+
+    public class CalcService
+    {
+        public static string Add(int a, int b) => (a + b).ToString();
+        public static string Sub(int a, int b) => (a - b).ToString();
+        public static string Mult(int a, int b) => (a * b).ToString();
+        public static string Div(int a, int b) => b != 0 ? (a / b).ToString() : "Infinity";
+    }
+}
