233ab25e5c6dbd8f73f0659bc60f4c8fc390811b
shit, it's not google <k1merpro@gmail.com>
2022-04-15 01:30:32 +0700

    2.4 Done!

--- Backend/Web2.4/Web2.4/Program.cs
+++ Backend/Web2.4/Web2.4/Program.cs
@@ -1,5 +1,9 @@
+using Web2._4.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddAccountService();
 
 var app = builder.Build();
 
